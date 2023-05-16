89c8fc9457930e73986c978ea25479bc491f2ee9
Nina Palma <ninapalmadev@gmail.com>
2023-05-16 18:55:25 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
+const minChar = 2;
 class Search extends React.Component {
+  state = {
+    search: '',
+  };
+
+  handleInputChange = ({ target }) => {
+    this.setState({
+      search: target.value,
+    });
+  };
+
   render() {
+    const { search } = this.state;
     return (
       <div data-testid="page-search">
-        <h1>Search</h1>
+        <form action="">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            id="search"
+            placeholder="Busca"
+            onChange={ this.handleInputChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ search.length < minChar }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }

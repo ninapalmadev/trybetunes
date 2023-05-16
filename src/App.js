5558a05d47d88446ab47b869678eb5657de67f10
Nina Palma <ninapalmadev@gmail.com>
2023-05-16 18:32:58 -0300

    corrige requisto 3

--- src/App.js
+++ src/App.js
@@ -16,17 +16,11 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" component={ Login } />
-
           <Route exact path="/search" component={ Search } />
-
           <Route exact path="/album/:id" component={ Album } />
-
           <Route exact path="/favorites" component={ Favorites } />
-
           <Route exact path="/profile" component={ Profile } />
-
           <Route exact path="/profile/edit" component={ ProfileEdit } />
-
           <Route exact path="" component={ NotFound } />
         </Switch>
       </BrowserRouter>

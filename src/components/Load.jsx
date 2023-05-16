4b9ffb2bf1b499f0efab326f544a68c9f7de8e14
Nina Palma <ninapalmadev@gmail.com>
2023-05-16 16:47:05 -0300

    requisito 2

---
+++ src/components/Load.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Load() {
+  return <h1>Carregando...</h1>;
+}
+
+export default Load;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Load from '../components/Load';
+
+const minChar = 3;
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    loading: false,
+    redirect: false,
+  };
+
+  handleInputChange = ({ target }) => {
+    this.setState({
+      name: target.value,
+    });
+  };
+
+  handleButtonSubmit = async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    this.setState({ loading: true });
+
+    try {
+      await createUser({ name });
+      this.setState({ loading: false, redirect: true });
+    } catch (err) {
+      console.error('Erro ao criar usuário:', err);
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
+    const { name, loading, redirect } = this.state;
+    if (redirect) return <Redirect to="/search" />;
     return (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        <h1>Trybe Tunes</h1>
+        <form>
+          <input
+            type="text"
+            id="name"
+            value={ name }
+            data-testid="login-name-input"
+            placeholder="Nome"
+            onChange={ this.handleInputChange }
+          />
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ name.length < minChar }
+            onClick={ this.handleButtonSubmit }
+          >
+            Entrar
+          </button>
+        </form>
+        {loading && <Load />}
       </div>
     );
   }

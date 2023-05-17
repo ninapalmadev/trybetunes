f6878e71329336343f92ea445a14be7a923be600
Nina Palma <ninapalmadev@gmail.com>
2023-05-17 19:00:55 -0300

    requisito 7

---
+++ src/components/ MusicCard.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+class MusicCard extends React.Component {
+  render() {
+    const { previewUrl, trackName } = this.props;
+    return (
+      <div>
+        <h2>
+          {trackName}
+        </h2>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+          .
+        </audio>
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  previewUrl: PropTypes.string.isRequired,
+  trackName: PropTypes.string.isRequired,
+};
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,69 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import getMusics from '../services/musicsAPI';
+import Load from '../components/Load';
+import MusicCard from '../components/ MusicCard';
 
 class Album extends React.Component {
+  state = {
+    album: [],
+    musics: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.getAlbumSongs();
+  }
+
+  getAlbumSongs = async () => {
+    const { match: { params: { id } } } = this.props;
+    const [album, ...musics] = await getMusics(id);
+    console.log(album);
+
+    this.setState({
+      album,
+      musics,
+      loading: false,
+    });
+  };
+
   render() {
+    const { album, musics, loading } = this.state;
     return (
       <div data-testid="page-album">
-        <h1>Album</h1>
+        <h1>
+          {`${album.artistName} -  ${album.collectionName} ` }
+        </h1>
+        {
+          loading ? (<Load />) : (
+            <section>
+              <h2 data-testid="artist-name">
+                {album.artistName}
+              </h2>
+              <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+              <p data-testid="album-name">
+                {album.collectionName}
+              </p>
+            </section>
+          )
+        }
+        <section>
+          {musics.map(
+            ({ previewUrl, trackName }, index) => (
+              <MusicCard
+                key={ index }
+                trackName={ trackName }
+                previewUrl={ previewUrl }
+              />
+            ),
+          )}
+        </section>
       </div>
     );
   }
 }
 
+Album.propTypes = {
+  match: PropTypes.shape().isRequired,
+};
 export default Album;
